9696d5a10e33e0f8b641804b18b39f0e4a61c4cd
Jiří Stránský <jistr@jistr.com>
2015-01-05 23:41:00 +0100

    Move yum update to the manifest file

--- build.sh
+++ build.sh
@@ -17,8 +17,6 @@ function create_builder_params() {
         "--password" "vagrant:password:vagrant"
         "--write" "/root/vagrant_pub_key:${VAGRANT_PUB_KEY}"
         "--install" "rsync,nfs-utils"  # for Vagrant file transfer
-        "--run-command" "yum -y update"
-        "--run-command" "yum clean all"
         "--run" "files/prepare_vm.sh"
         "--run-command" "touch /.autorelabel"
     )
